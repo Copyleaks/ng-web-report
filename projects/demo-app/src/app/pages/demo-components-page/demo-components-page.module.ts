5d4b2f90829fb1aaf058697ac004811f961ddd8d
Eyad Heib <eyadh@copyleaks.com>
2023-10-04 12:52:40 +0300

    fix

--- projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
+++ projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { DemoComponentsPageRoutingModule } from './demo-components-page-routing.module';
 import { DemoComponentsPageComponent } from './demo-components-page.component';
-import { ReportAlertsContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-alerts-container/report-alerts-container.module';
 
 @NgModule({
 	declarations: [DemoComponentsPageComponent],
-	imports: [CommonModule, DemoComponentsPageRoutingModule, ReportAlertsContainerModule],
+	imports: [CommonModule, DemoComponentsPageRoutingModule],
 })
 export class DemoComponentsPageModule {}

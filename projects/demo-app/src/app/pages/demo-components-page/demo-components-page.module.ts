a72cf7497976b77ed6352ee120d34c855b458e73
Eyad Heib <eyadh@copyleaks.com>
2023-10-09 09:03:01 +0300

    add to demo

--- projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
+++ projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DemoComponentsPageRoutingModule } from './demo-components-page-routing.module';
 import { DemoComponentsPageComponent } from './demo-components-page.component';
+import { PoweredByModule } from 'projects/copyleaks-web-report/src/lib/components/powered-by/powered-by.module';
 
 @NgModule({
 	declarations: [DemoComponentsPageComponent],
-	imports: [CommonModule, DemoComponentsPageRoutingModule],
+	imports: [CommonModule, DemoComponentsPageRoutingModule, PoweredByModule],
 })
 export class DemoComponentsPageModule {}

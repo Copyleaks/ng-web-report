e7d92cd6357f2f54597a8baa81af1900120849d3
safaamasha <safaa@copyleaks.com>
2023-10-11 12:56:49 +0300

    add base component for the shared code between the layouts

---
+++ projects/copyleaks-web-report/src/lib/components/layouts/base/report-layout-base.component.ts
@@ -0,0 +1,52 @@
+import { Renderer2 } from '@angular/core';
+import { ReportDataService } from '../../../services/report-data.service';
+import iframeJsScript from '../../../utils/one-to-many-iframe-logic';
+import { COPYLEAKS_REPORT_IFRAME_STYLES } from '../../../constants/iframe-styles.constants';
+import { Match } from '../../../models/report-matches.models';
+import { ReportMatchesService } from '../../../services/report-matches.service';
+import { IReportViewEvent } from '../../../models/report-view.models';
+import { ReportViewService } from '../../../services/report-view.service';
+import * as helpers from '../../../utils/report-match-helpers';
+import { ReportMatchHighlightService } from '../../../services/report-match-highlight.service';
+
+export abstract class ReportLayoutBaseComponent {
+	iframeStyle: string = COPYLEAKS_REPORT_IFRAME_STYLES;
+	iframeJsScript: string = iframeJsScript;
+	isHtmlView: boolean;
+
+	abstract get rerendered(): boolean;
+
+	constructor(
+		protected reportDataSvc: ReportDataService,
+		protected reportViewSvc: ReportViewService,
+		protected matchSvc: ReportMatchesService,
+		protected renderer: Renderer2,
+		protected highlightSvc: ReportMatchHighlightService
+	) {}
+
+	onReportViewChange(event: IReportViewEvent) {
+		this.isHtmlView = event.isHtmlView;
+		this.reportViewSvc.reportViewMode$.next(event);
+	}
+
+	/**
+	 * Render list of matches in the iframe's HTML
+	 * @param matches the matches to render
+	 */
+	protected _getRenderedMatches(matches: Match[] | null, originalHtml: string) {
+		if (this.rerendered || !matches || !originalHtml) return null;
+
+		const html = helpers.getRenderedMatches(matches, originalHtml);
+		if (!html) return null;
+
+		const css = this.renderer.createElement('style') as HTMLStyleElement;
+		css.textContent = this.iframeStyle;
+		const iframeStyle = css.outerHTML;
+
+		const js = this.renderer.createElement('script') as HTMLScriptElement;
+		js.textContent = this.iframeJsScript;
+		const iframeJsScript = js.outerHTML;
+
+		return html + iframeStyle + iframeJsScript;
+	}
+}
--- projects/copyleaks-web-report/src/lib/components/layouts/one-to-many/one-to-many-report-layout-desktop/one-to-many-report-layout-desktop.component.ts
+++ projects/copyleaks-web-report/src/lib/components/layouts/one-to-many/one-to-many-report-layout-desktop/one-to-many-report-layout-desktop.component.ts
@@ -2,66 +2,74 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ReportDataService } from '../../../../services/report-data.service';
 import iframeJsScript from '../../../../utils/one-to-many-iframe-logic';
 import { COPYLEAKS_REPORT_IFRAME_STYLES } from '../../../../constants/iframe-styles.constants';
-import { Match, MatchType, SlicedMatch } from '../../../../models/report-matches.models';
-import { EExcludeReason } from '../../../../enums/copyleaks-web-report.enums';
+import { Match, SlicedMatch } from '../../../../models/report-matches.models';
 import { ReportMatchesService } from '../../../../services/report-matches.service';
 import { PostMessageEvent } from '../../../../models/report-iframe-events.models';
 import { IScanSource, ResultPreview } from '../../../../models/report-data.models';
 import { IReportViewEvent } from '../../../../models/report-view.models';
 import { ReportViewService } from '../../../../services/report-view.service';
+import * as helpers from '../../../../utils/report-match-helpers';
+import { ReportLayoutBaseComponent } from '../../base/report-layout-base.component';
+import { ReportMatchHighlightService } from 'projects/copyleaks-web-report/src/lib/services/report-match-highlight.service';
 
 @Component({
 	selector: 'copyleaks-one-to-many-report-layout-desktop',
 	templateUrl: './one-to-many-report-layout-desktop.component.html',
 	styleUrls: ['./one-to-many-report-layout-desktop.component.scss'],
 })
-export class OneToManyReportLayoutDesktopComponent implements OnInit {
+export class OneToManyReportLayoutDesktopComponent extends ReportLayoutBaseComponent implements OnInit {
 	hideRightSection = false;
 
-	isHtmlView: boolean = true;
 	reportCrawledVersion: IScanSource;
 	iframeHtml: string;
-	iframeStyle = COPYLEAKS_REPORT_IFRAME_STYLES;
-	iframeJsScript = iframeJsScript;
 	reportMatches: Match[];
 
-	rerendered: any;
 	contentTextMatches: SlicedMatch[][];
 	numberOfPages: number;
 	currentPageSource: number;
+	oneToOneRerendered: boolean;
 
 	get numberOfWords(): number | undefined {
-		return this._reportDataSvc.scanResultsPreviews?.scannedDocument?.totalWords;
+		return this.reportDataSvc.scanResultsPreviews?.scannedDocument?.totalWords;
+	}
+
+	override get rerendered(): boolean {
+		return this.oneToOneRerendered;
 	}
 
 	constructor(
-		private _reportDataSvc: ReportDataService,
-		private _reportViewSvc: ReportViewService,
-		private _matchSvc: ReportMatchesService,
-		private _renderer: Renderer2
-	) {}
+		reportDataSvc: ReportDataService,
+		reportViewSvc: ReportViewService,
+		matchSvc: ReportMatchesService,
+		renderer: Renderer2,
+		highlightSvc: ReportMatchHighlightService
+	) {
+		super(reportDataSvc, reportViewSvc, matchSvc, renderer, highlightSvc);
+	}
 
 	ngOnInit(): void {
-		this._reportDataSvc.crawledVersion$.subscribe(data => {
+		this.reportDataSvc.crawledVersion$.subscribe(data => {
 			if (data) {
 				this.reportCrawledVersion = data;
 				this.numberOfPages = data.text?.pages?.startPosition?.length ?? 1;
 			}
 		});
 
-		this._matchSvc.originalHtmlMatches$.subscribe(data => {
-			if (data) {
-				this._renderMatches(data);
+		this.matchSvc.originalHtmlMatches$.subscribe(data => {
+			const updatedHtml = this._getRenderedMatches(data, this.reportCrawledVersion?.html.value);
+			if (updatedHtml && data) {
+				this.iframeHtml = updatedHtml;
+				this.oneToOneRerendered = true;
 			}
 		});
 
-		this._matchSvc.originalTextMatches$.subscribe(data => {
+		this.matchSvc.originalTextMatches$.subscribe(data => {
 			if (data) {
 				this.contentTextMatches = data;
 			}
 		});
 
-		this._reportViewSvc.reportViewMode$.subscribe(data => {
+		this.reportViewSvc.reportViewMode$.subscribe(data => {
 			if (!data) return;
 			this.isHtmlView = data.isHtmlView;
 			this.currentPageSource = data.sourcePageIndex;
@@ -74,16 +82,16 @@ export class OneToManyReportLayoutDesktopComponent implements OnInit {
 				const selectedMatch = message.index !== -1 ? this.reportMatches[message.index] : null;
 				let viewedResults: ResultPreview[] = [];
 				viewedResults = [
-					...(this._reportDataSvc.scanResultsPreviews?.results?.internet?.filter(item =>
+					...(this.reportDataSvc.scanResultsPreviews?.results?.internet?.filter(item =>
 						selectedMatch?.ids?.includes(item.id)
 					) ?? []),
-					...(this._reportDataSvc.scanResultsPreviews?.results?.batch?.filter(item =>
+					...(this.reportDataSvc.scanResultsPreviews?.results?.batch?.filter(item =>
 						selectedMatch?.ids?.includes(item.id)
 					) ?? []),
-					...(this._reportDataSvc.scanResultsPreviews?.results?.database?.filter(item =>
+					...(this.reportDataSvc.scanResultsPreviews?.results?.database?.filter(item =>
 						selectedMatch?.ids?.includes(item.id)
 					) ?? []),
-					...(this._reportDataSvc.scanResultsPreviews?.results?.repositories?.filter(item =>
+					...(this.reportDataSvc.scanResultsPreviews?.results?.repositories?.filter(item =>
 						selectedMatch?.ids?.includes(item.id)
 					) ?? []),
 				];
@@ -98,50 +106,5 @@ export class OneToManyReportLayoutDesktopComponent implements OnInit {
 			default:
 				console.error('unknown event', message);
 		}
-	}
-
-	onReportViewChange(event: IReportViewEvent) {
-		this.isHtmlView = event.isHtmlView;
-		this._reportViewSvc.reportViewMode$.next(event);
-	}
-
-	/**
-	 * Render list of matches in the iframe's HTML
-	 * @param matches the matches to render
-	 */
-	private _renderMatches(matches: Match[]) {
-		if (this.rerendered) return;
-
-		this.reportMatches = matches;
-
-		const html = matches.reduceRight((prev: string, curr: Match, i: number) => {
-			let slice = this.reportCrawledVersion?.html?.value?.substring(curr.start, curr.end);
-			switch (curr.type) {
-				case MatchType.excluded:
-					if (curr.reason === EExcludeReason.PartialScan) {
-						slice = `<span exclude-partial-scan data-type="${curr.type}" data-index="${i}">${slice}</span>`;
-					} else {
-						slice = `<span exclude>${slice}</span>`;
-					}
-					break;
-				case MatchType.none:
-					break;
-				default:
-					slice = `<span match data-type="${curr.type}" data-index="${i}" data-gid="${curr.gid}">${slice}</span>`;
-					break;
-			}
-			return slice ? slice?.concat(prev) : '';
-		}, '');
-
-		const css = this._renderer.createElement('style') as HTMLStyleElement;
-		css.textContent = this.iframeStyle;
-		this.iframeStyle = css.outerHTML;
-
-		const js = this._renderer.createElement('script') as HTMLScriptElement;
-		js.textContent = this.iframeJsScript;
-		this.iframeJsScript = js.outerHTML;
-
-		this.iframeHtml = html + this.iframeStyle + this.iframeJsScript;
-		this.rerendered = true;
 	}
 }

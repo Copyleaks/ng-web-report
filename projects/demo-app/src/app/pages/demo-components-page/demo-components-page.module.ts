f439b3a8c70cae27d2c77fbd8d156cd06d3246c3
AihamAR <aihama@copyleaks.com>
2023-10-11 11:57:17 +0300

    fixes

--- projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
+++ projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReportAlertsContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-alerts-container/report-alerts-container.module';
+import { PoweredByModule } from 'projects/copyleaks-web-report/src/lib/components/powered-by/powered-by.module';
 import { DemoComponentsPageRoutingModule } from './demo-components-page-routing.module';
 import { DemoComponentsPageComponent } from './demo-components-page.component';
-import { PoweredByModule } from 'projects/copyleaks-web-report/src/lib/components/powered-by/powered-by.module';
-import { ReportAlertsContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-alerts-container/report-alerts-container.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
 	declarations: [DemoComponentsPageComponent],

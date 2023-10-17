1d6b3d2e03423f4cd1a404becf29182eba8b8f99
safaamasha <safaa@copyleaks.com>
2023-10-17 15:11:35 +0300

    switch the alerts expantion to use material expansion component with animation

--- projects/copyleaks-web-report/src/lib/components/containers/report-alerts-container/report-alerts-container.module.ts
+++ projects/copyleaks-web-report/src/lib/components/containers/report-alerts-container/report-alerts-container.module.ts
@@ -7,10 +7,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatChipsModule } from '@angular/material/chips';
 import { AuthorAlertCardComponent } from './components/author-alert-card/author-alert-card.component';
 import { ReportViewService } from '../../../services/report-view.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
 	declarations: [ReportAlertsContainerComponent, AlertCardComponent, AuthorAlertCardComponent],
-	imports: [CommonModule, MatIconModule, MatChipsModule, FlexLayoutModule],
+	imports: [CommonModule, MatIconModule, MatChipsModule, FlexLayoutModule, MatButtonModule, MatExpansionModule],
 	exports: [ReportAlertsContainerComponent, AlertCardComponent, AuthorAlertCardComponent],
 	providers: [ReportViewService],
 })

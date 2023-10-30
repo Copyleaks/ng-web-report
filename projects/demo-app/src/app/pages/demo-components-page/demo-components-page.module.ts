3ae42dc2fd9d3aed291441579f57ce63716aef29
Eyad Heib <eyadh@copyleaks.com>
2023-10-30 08:45:16 +0200

    fix

--- projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
+++ projects/demo-app/src/app/pages/demo-components-page/demo-components-page.module.ts
@@ -1,22 +1,20 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReportAlertsContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-alerts-container/report-alerts-container.module';
+import { ReportResultsContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-results-container/report-results-container.module';
+import { ReportResultsItemContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-results-item-container/report-results-item-container.module';
+import { CrCustomTabItemModule } from 'projects/copyleaks-web-report/src/lib/components/core/cr-custom-tabs/components/cr-custom-tab-item/cr-custom-tab-item.module';
+import { CrCustomTabsModule } from 'projects/copyleaks-web-report/src/lib/components/core/cr-custom-tabs/cr-custom-tabs.module';
+import { CrPoweredByModule } from 'projects/copyleaks-web-report/src/lib/components/core/cr-powered-by/cr-powered-by.module';
+import { FilterResultDailogModule } from 'projects/copyleaks-web-report/src/lib/dialogs/filter-result-dailog/filter-result-dailog.module';
+import { SimilarityPipeModule } from 'projects/copyleaks-web-report/src/lib/pipes/similarity-pipe/similarity-pipe.module';
+import { ReportNgTemplatesService } from 'projects/copyleaks-web-report/src/lib/services/report-ng-templates.service';
+import { ReportViewService } from 'projects/copyleaks-web-report/src/lib/services/report-view.service';
 import { DemoComponentsPageRoutingModule } from './demo-components-page-routing.module';
 import { DemoComponentsPageComponent } from './demo-components-page.component';
-import { SimilarityPipeModule } from 'projects/copyleaks-web-report/src/lib/pipes/similarity-pipe/similarity-pipe.module';
-import { ReportResultsContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-results-container/report-results-container.module';
-import { FilterResultDailogModule } from 'projects/copyleaks-web-report/src/lib/dialogs/filter-result-dailog/filter-result-dailog.module';
-import { ReportViewService } from 'projects/copyleaks-web-report/src/lib/services/report-view.service';
-import { ReportResultsItemContainerModule } from 'projects/copyleaks-web-report/src/lib/components/containers/report-results-item-container/report-results-item-container.module';
-import { CrPoweredByModule } from 'projects/copyleaks-web-report/src/lib/components/core/cr-powered-by/cr-powered-by.module';
-import { CrCustomTabsModule } from 'projects/copyleaks-web-report/src/lib/components/core/cr-custom-tabs/cr-custom-tabs.module';
-import { CrCustomTabItemModule } from 'projects/copyleaks-web-report/src/lib/components/core/cr-custom-tabs/components/cr-custom-tab-item/cr-custom-tab-item.module';
-import { ReportNgTemplatesService } from 'projects/copyleaks-web-report/src/lib/services/report-ng-templates.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ReportMatchHighlightService } from 'projects/copyleaks-web-report/src/lib/services/report-match-highlight.service';
-import { ReportDataService } from 'projects/copyleaks-web-report/src/lib/services/report-data.service';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
 	declarations: [DemoComponentsPageComponent],
@@ -36,6 +34,6 @@ import { HttpClientModule } from '@angular/common/http';
 		ReactiveFormsModule,
 		HttpClientModule,
 	],
-	providers: [ReportViewService, ReportNgTemplatesService, ReportMatchHighlightService, ReportDataService],
+	providers: [ReportViewService, ReportNgTemplatesService],
 })
 export class DemoComponentsPageModule {}

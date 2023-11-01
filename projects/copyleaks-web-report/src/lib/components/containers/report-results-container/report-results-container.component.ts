1f27609bfebe06e2726d86cfae67810cd7331717
Bayan Abuawad <bayana@copyleaks.com>
2023-11-01 14:35:02 +0200

    result container changes with safa

--- projects/copyleaks-web-report/src/lib/components/containers/report-results-container/report-results-container.component.ts
+++ projects/copyleaks-web-report/src/lib/components/containers/report-results-container/report-results-container.component.ts
@@ -1,5 +1,4 @@
 import {
-	AfterViewInit,
 	Component,
 	ElementRef,
 	HostBinding,
@@ -7,13 +6,11 @@ import {
 	OnChanges,
 	OnInit,
 	Renderer2,
-	SimpleChanges,
 	ViewChild,
+	SimpleChange,
+	SimpleChanges,
 } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 import { EResponsiveLayoutType } from '../../../enums/copyleaks-web-report.enums';
-import { untilDestroy } from '../../../utils/until-destroy';
 import { EnumNavigateMobileButton } from '../report-results-item-container/components/models/report-result-item.enum';
 import { IResultItem } from '../report-results-item-container/components/models/report-result-item.models';
 import { IResultsActions } from './components/results-actions/models/results-actions.models';
@@ -23,7 +20,7 @@ import { IResultsActions } from './components/results-actions/models/results-act
 	templateUrl: './report-results-container.component.html',
 	styleUrls: ['./report-results-container.component.scss'],
 })
-export class ReportResultsContainerComponent implements OnInit, AfterViewInit, OnChanges {
+export class ReportResultsContainerComponent implements OnInit, OnChanges {
 	@HostBinding('style.display')
 	displayProp = 'flex';
 
@@ -34,7 +31,7 @@ export class ReportResultsContainerComponent implements OnInit, AfterViewInit, O
 	 */
 	@Input() flexGrow: number;
 	@Input() reportResponsive: EResponsiveLayoutType;
-	@Input() allResultsItem: IResultItem[] = [];
+	@Input() allResults: IResultItem[] = [];
 	@Input() resultsActions: IResultsActions;
 	@Input() isMobile: boolean;
 
@@ -48,11 +45,7 @@ export class ReportResultsContainerComponent implements OnInit, AfterViewInit, O
 	@ViewChild('resultsContainer', { read: ElementRef }) public resultsContainer: ElementRef;
 	@ViewChild('resultitem', { read: ElementRef }) public resultitem: ElementRef;
 
-	private _startingIndex: number = 0;
-	private _pageSize: number = 10;
-	private _currentPage: number = 1;
-
-	resultItemList: IResultItem[];
+	displayedResults: IResultItem[];
 	lastItemLoading: boolean = false;
 
 	navigateMobileButton: EnumNavigateMobileButton;
@@ -60,83 +53,24 @@ export class ReportResultsContainerComponent implements OnInit, AfterViewInit, O
 	showMatChip: boolean = true;
 	searchedValue: string;
 
-	get EndingIndex(): number {
-		return this._startingIndex + this._pageSize * this._currentPage;
-	}
-
 	get allResultsItemLength() {
-		return this.allResultsItem?.length;
+		return this.allResults?.length;
 	}
 
 	constructor(private _renderer: Renderer2) {}
 
+	ngOnChanges(change: SimpleChanges) {
+		if (change['allResults']?.currentValue) {
+			this.searchedValue = '';
+			this.displayedResults = this.allResults;
+		}
+	}
+
 	ngOnInit(): void {
-		if (this.flexGrow !== undefined && this.flexGrow !== null) this.flexGrowProp = this.flexGrow;
-
-		if (!this.allResultsItem || this.allResultsItem?.length === 0) this.lastItemLoading = false;
-
-		if (this.allResultsItemLength > this._pageSize) {
-			this.resultItemList = this.allResultsItem.slice(this._startingIndex, this.EndingIndex);
-		} else {
-			this.resultItemList = this.allResultsItem;
+		if (this.flexGrow !== undefined && this.flexGrow !== null) {
+			this.flexGrowProp = this.flexGrow;
 		}
 		this.navigateMobileButton = EnumNavigateMobileButton.FirstButton;
-	}
-
-	ngAfterViewInit(): void {
-		fromEvent(this.resultsContainer.nativeElement, 'scroll')
-			.pipe(debounceTime(200), untilDestroy(this))
-			.subscribe((e: any) => this.onTableScroll(e));
-	}
-
-	ngOnChanges(changes: SimpleChanges): void {
-		if ('allResultsItem' in changes) {
-			this.searchedValue = '';
-			this._startingIndex = 0;
-			this._pageSize = 10;
-			this._currentPage = 1;
-			if (this.allResultsItemLength > this._pageSize) {
-				this.resultItemList = this.allResultsItem.slice(this._startingIndex, this.EndingIndex);
-			} else {
-				this.resultItemList = this.allResultsItem;
-			}
-
-			if (!this.allResultsItem || this.allResultsItem?.length === 0) this.lastItemLoading = false;
-		}
-	}
-
-	private onTableScroll(e: any): void {
-		const scrollThreshold = 200;
-
-		if (!!this.searchedValue) return;
-
-		if (this.isMobile) {
-			const tableViewWidth = e.target.offsetWidth;
-			const tableScrollWidth = e.target.scrollWidth;
-			const scrollLocation = e.target.scrollLeft;
-			const scrollRightLimit = tableScrollWidth - tableViewWidth - scrollThreshold;
-			this.updateNavigateButton(scrollLocation);
-			if (scrollLocation > scrollRightLimit && this.EndingIndex < this.allResultsItemLength) {
-				this._currentPage += 1;
-				this.lastItemLoading = true;
-				this.resultItemList = this.allResultsItem.slice(this._startingIndex, this.EndingIndex);
-				this.scrollTo(tableScrollWidth / 2 + tableViewWidth);
-			}
-		} else {
-			const tableViewHeight = e.target.offsetHeight;
-			const tableScrollHeight = e.target.scrollHeight;
-			const scrollLocation = e.target.scrollTop;
-			const scrollDownLimit = tableScrollHeight - tableViewHeight - scrollThreshold;
-			if (scrollLocation > scrollDownLimit && this.EndingIndex < this.allResultsItemLength) {
-				this._currentPage += 1;
-				this.lastItemLoading = true;
-				this.resultItemList = this.allResultsItem.slice(this._startingIndex, this.EndingIndex);
-				this.scrollTo(tableScrollHeight / 2 + tableViewHeight);
-			}
-		}
-		if (this.EndingIndex >= this.allResultsItemLength) {
-			this.lastItemLoading = false;
-		}
 	}
 
 	private scrollTo(position: number): void {
@@ -153,7 +87,7 @@ export class ReportResultsContainerComponent implements OnInit, AfterViewInit, O
 
 	//#region navigate mobile button
 	navigateButton(navigateButton: EnumNavigateMobileButton) {
-		const navigateNum = this.resultItemList?.length / 5;
+		const navigateNum = this.displayedResults?.length / 5;
 		const resultitemWidth = this.resultitem.nativeElement.offsetWidth;
 		const viewWidth = resultitemWidth * navigateNum;
 		switch (navigateButton) {
@@ -191,7 +125,7 @@ export class ReportResultsContainerComponent implements OnInit, AfterViewInit, O
 	}
 
 	updateNavigateButton(scrollLocation: number) {
-		const navigateNum = this.resultItemList?.length / 5;
+		const navigateNum = this.displayedResults?.length / 5;
 		const resultitemWidth = this.resultitem.nativeElement.offsetWidth;
 		const viewWidth = resultitemWidth * navigateNum;
 		if (0 <= scrollLocation && viewWidth > scrollLocation) {
@@ -211,25 +145,18 @@ export class ReportResultsContainerComponent implements OnInit, AfterViewInit, O
 		this.searchedValue = value;
 
 		if (!value || value === '') {
-			if (!this.allResultsItem || this.allResultsItem?.length === 0) this.lastItemLoading = false;
-
-			if (this.allResultsItemLength > this._pageSize) {
-				this.resultItemList = this.allResultsItem.slice(this._startingIndex, this.EndingIndex);
-			} else {
-				this.resultItemList = this.allResultsItem;
-			}
+			this.displayedResults = this.allResults;
 			return;
 		}
 
 		value = value.toLowerCase();
 
-		this.resultItemList = this.allResultsItem.filter(
+		this.displayedResults = this.allResults.filter(
 			r =>
 				r.resultPreview.introduction.toLowerCase().includes(value) ||
 				r.resultPreview.title.toLowerCase().includes(value) ||
 				(r.resultPreview.url && r.resultPreview.url.toLowerCase().includes(value))
 		);
-		this.lastItemLoading = false;
 	}
 
 	//#endregion

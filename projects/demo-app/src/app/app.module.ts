6bdc4bdc21a55154b6c119457d83c5d1f61056c5
AihamAR <aihama@copyleaks.com>
2023-11-29 16:01:33 +0200

    remove httpclientmodule

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, DemoSidenavModule],
+	imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, DemoSidenavModule, HttpClientModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })

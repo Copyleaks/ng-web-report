b8e3cd4812da2ed4a4c2b25aa13fbac8010841bf
safaamasha <safaa@copyleaks.com>
2024-05-02 18:05:13 +0300

    add missing exclusion tooltip messages

--- projects/copyleaks-web-report/src/lib/constants/report-exclude.constants.ts
+++ projects/copyleaks-web-report/src/lib/constants/report-exclude.constants.ts
@@ -4,10 +4,14 @@ import '@angular/localize/init';
 export const EXCLUDE_MESSAGE: { [key: string]: string } = {
 	1: $localize`Quotations are omitted according to your settings`,
 	2: $localize`References are omitted according to your settings`,
+	3: $localize`Headers are omitted according to your settings`,
+	4: $localize`Footers are omitted according to your settings`,
 	5: $localize`HTML templates are omitted according to your settings`,
 	6: $localize`Tables of content are omitted according to your settings`,
 	7: $localize`Source code comments are omitted according to your settings`,
 	0: $localize`Sensitive data are hidden according to your settings`,
 	8: $localize`This text was not scanned because there were not enough pages. Please upgrade your plan in order to scan the entire document.`,
 	9: $localize`Citations are omitted according to your settings`,
+	10: $localize`Titles are omitted according to your settings`,
+	11: $localize`This text was not scanned because it matches content from your uploaded exclusion templates.`,
 };

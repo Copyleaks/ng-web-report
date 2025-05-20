4a9196dc97c1f4897ce97c92f52147135b5b50d1
safaamasha <safaa@copyleaks.com>
2025-05-20 13:37:08 +0300

    feature/WEB-1641-rename-repository

--- projects/copyleaks-web-report/src/lib/components/containers/report-results-item-container/components/report-expand-result-item/report-expand-result-item.component.ts
+++ projects/copyleaks-web-report/src/lib/components/containers/report-results-item-container/components/report-expand-result-item/report-expand-result-item.component.ts
@@ -58,11 +58,11 @@ export class ReportExpandResultItemComponent implements OnInit, OnChanges {
 				case EResultPreviewType.Internet:
 					return 'Internet Result';
 				case EResultPreviewType.Database:
-					return 'Internal Database Result';
+					return 'Shared Data Hub Result';
 				case EResultPreviewType.Batch:
 					return 'Batch Result';
 				case EResultPreviewType.Repositroy:
-					return 'Repository Result';
+					return 'Private Cloud Hub Result';
 				default:
 					return '';
 			}
